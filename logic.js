521ebcc1477381d936a2610c5880370f78e5ec1e
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-04-30 18:31:20 +0500

    Logic done. needs cleaning. CSS pending

--- logic.js
+++ logic.js
@@ -16,36 +16,32 @@ function populateStorage() {
 }
 
 function render() {
+    /* renders directly from localStorage. localStorage updated everytime library is so convenient 
+    to just render from there instead of checking both to see which is more up to date*/
     if (store.length >= 1) {
         for (const book of JSON.parse(store.getItem('library'))) {
             libraryDisplay.innerHTML += `
                 <div class="library-item">
-                    <h1><span class="bold">Title:</span> ${book.title}</h1>
-                    <p><span class="bold">Author:</span> ${book.author}</p>
-                    <p><span class="bold">Pages:</span> ${book.pages}</p>
-                    <p><span class="bold">Read:</span> ${book.read}</p>
-                    <button>Delete Book</button>
+                    <h1><span class="bold">Title:&nbsp;</span> ${book.title}</h1>
+                    <p><span class="bold">Author:&nbsp;</span> ${book.author}</p>
+                    <p><span class="bold">Pages:&nbsp;</span> ${book.pages}</p>
+                    <p><span class="bold">Read:&nbsp;</span> ${book.read ? 'Yes' : 'No'}</p>
+                    <button class="book-delete-btn" id="${findIndex(book._id)}" onClick="removeBookFromLibrary(this.id)">Delete Book</button>
+                    <button id="${book._id}" onClick="changeReadStatus(this.id)">Change Read Status</button>
                 </div>`
         }
     }	
 }
 
-// populateStorage();
-render();
-
-
+render(); // initial render on page load
 
 class Book {
     constructor(title, author, pages, read) {
-        this._id = keyGen();
+        this._id = `_${Math.random().toString(36).substr(2,10)}`;
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
-    }
-
-    get id() {
-        return this._id;
     }
 
     toggleReadStatus() {
@@ -53,48 +49,49 @@ class Book {
 	}
 }
 
-
-
-
-
 function addBookToLibrary(book) {
     if (formVerify()) {
+        // won't proceed until form is filled out
         let book = new Book(document.getElementById('enter-name').value,
                             document.getElementById('enter-author').value,
                             Number(document.getElementById('enter-pages').value),
                             Boolean(document.querySelector('input[name="read"]:checked').value));
-		library.push(book);
+        library.push(book);
+        populateStorage();
+        libraryDisplay.innerHTML = '';
+        render(); 
     } else {
         alert('Please fill out all the fields!');
     }
-    console.log(library);
+    document.getElementById('new-book').reset(); // clear form
+}
+
+function removeBookFromLibrary(domID) {
+    library.splice(domID, 1);
     populateStorage();
     libraryDisplay.innerHTML = '';
     render(); 
-    document.getElementById('new-book').reset();
 }
 
 addBookBtn.addEventListener('click', addBookToLibrary);
 
-
-
 function formVerify() {
     // prevents form with empty fields from being submitted
-    // function working as expected
     return (document.getElementById('enter-name').value !== '' && 
             document.getElementById('enter-author').value !== '' && 
             document.getElementById('enter-pages').value !== '');
 } 
 
-function clearForm() {
-    document.getElementById('enter-name').value = ''; 
-    document.getElementById('enter-author').value = ''; 
-    document.getElementById('enter-pages').value = '';
+function findIndex(id) {
+    for (const element of library) {
+        if (id === element._id) return library.indexOf(element);
+    }
 }
 
-
-
-function keyGen() {
-    // generate a key for local storage
-    return `_${Math.random().toString(36).substr(2,10)}`;
+function changeReadStatus(book_id) {
+    const index = findIndex(book_id);
+    library[index].toggleReadStatus();
+    populateStorage();
+    libraryDisplay.innerHTML = '';
+    render(); 
 }
