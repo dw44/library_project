1d5866959e5664e8e99f4dbaa54de26aeec06ecd
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-05-01 11:10:33 +0500

    reworked logic to fix bugs

--- logic.js
+++ logic.js
@@ -1,97 +1,106 @@
-const library = []; 
 const store = window.localStorage;
-const libraryDisplay = document.querySelector('section[id="library"]');
+const libraryDisplay = document.getElementById('library');
 const addBookBtn = document.getElementById('submit-book');
+const library = [
+	// {_id: '_9jc8vpxlse', title: 'Eloquent JavaScript', author: 'Marijn Haverbeke', pages: 448, read: true},
+	// {_id: '_1n9qwxnt2q', title: 'The Principles of Object Oriented JavaScript', author: 'Nicholas Zakas', pages: 122, read: true},
+	// {_id: '_smrfz2ebvs', title: 'Introduction to Algorithms', author: 'CLRS', pages: 1313, read: false},
+	// {_id: '_nbwxaexu4o', title: 'C.O.D.E' , author: 'Charles Petzold', pages: 391, read: true},
+	// {_id: '_3bdmslmvim', title: 'Composing Software', author: 'Eric Elliot', pages: 229, read: false},
+	// {_id: '_n5b2s11skq', title: 'Red Birds', author: 'Mohammad Hanif', pages: 283, read: true}
+];
 
-// populates library with items in local storage  but only if there's something in local storage.
-if (store.length >= 1) { 
-    for (const book of JSON.parse(store.getItem('library'))) {
-        library.push(book);
-    }
-}    
-
-function populateStorage() {
-    // stores a snapshot of the current state of the library array to localStorage
-    store.setItem('library', JSON.stringify(library));
+function checkID(id) {
+	// Returns true if library contains item with the same '_id' attribute as the 'id' parameter
+	return library.some(book => book._id === id);
 }
 
-function render() {
-    /* renders directly from localStorage. localStorage updated everytime library is so convenient 
-    to just render from there instead of checking both to see which is more up to date*/
-    if (store.length >= 1) {
-        for (const book of JSON.parse(store.getItem('library'))) {
-            libraryDisplay.innerHTML += `
-                <div class="library-item">
-                    <h1><span class="bold">Title:&nbsp;</span> ${book.title}</h1>
-                    <p><span class="bold">Author:&nbsp;</span> ${book.author}</p>
-                    <p><span class="bold">Pages:&nbsp;</span> ${book.pages}</p>
-                    <p><span class="bold">Read:&nbsp;</span> ${book.read ? 'Yes' : 'No'}</p>
-                    <button class="book-delete-btn" id="${findIndex(book._id)}" onClick="removeBookFromLibrary(this.id)">Delete Book</button>
-                    <button id="${book._id}" onClick="changeReadStatus(this.id)">Change Read Status</button>
-                </div>`
-        }
-    }	
+function synchronize() {
+	/*Synchronizes localStorage with the library array. Copies library to store if localStorage is empty.
+		If it isn't, adds books stored in localStorage that don't exist in library to library. Once library
+		is populated, localStorage is cleared and repopulated with the updated library*/
+	if (store.length === 0) {
+		store.setItem('library', JSON.stringify(library));
+	} else {
+		for (const book of JSON.parse(store.getItem('library'))) {
+			if (!checkID(book._id)) {
+				library.push(book);
+			}
+		}
+	}
+	store.clear();
+	store.setItem('library', JSON.stringify(library));
 }
 
-render(); // initial render on page load
-
-class Book {
-    constructor(title, author, pages, read) {
-        this._id = `_${Math.random().toString(36).substr(2,10)}`;
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-
-    toggleReadStatus() {
-        this.read = !this.read;
+function render() { 
+	// synchronize library array and local storage prior to rendering.
+	synchronize();
+	// clear display area for books to prevent double rendering of previously rendered elements
+	libraryDisplay.innerHTML = '';
+	// actual rendering done from localStorage. 
+	for (const book of JSON.parse(store.getItem('library'))) {
+		libraryDisplay.innerHTML += `
+			<div class="library-item">
+				<h1><span class="bold">Title:&nbsp;</span> ${book.title}</h1>
+				<p><span class="bold">Author:&nbsp;</span> ${book.author}</p>
+				<p><span class="bold">Pages:&nbsp;</span> ${book.pages}</p>
+				<p><span class="bold">Read:&nbsp;</span> ${book.read ? 'Yes' : 'No'}</p>
+				<button class="book-delete-btn" id="${findIndex(book._id)}" onClick="removeBookFromLibrary(this.id)">Delete Book</button>
+				<button id="${book._id}" onClick="changeReadStatus(this.id)">Change Read Status</button>
+			</div>`
 	}
 }
 
-function addBookToLibrary(book) {
-    if (formVerify()) {
-        // won't proceed until form is filled out
-        let book = new Book(document.getElementById('enter-name').value,
-                            document.getElementById('enter-author').value,
-                            Number(document.getElementById('enter-pages').value),
-                            Boolean(document.querySelector('input[name="read"]:checked').value));
-        library.push(book);
-        populateStorage();
-        libraryDisplay.innerHTML = '';
-        render(); 
-    } else {
-        alert('Please fill out all the fields!');
-    }
-    document.getElementById('new-book').reset(); // clear form
+render();
+
+function Book(title, author, pages, read) {
+	this._id = `_${Math.random().toString(36).substr(2,10)}`;
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
 }
 
-function removeBookFromLibrary(domID) {
-    library.splice(domID, 1);
-    populateStorage();
-    libraryDisplay.innerHTML = '';
-    render(); 
+function formVerify() {
+	// prevents form with empty fields from being submitted
+	return (document.getElementById('enter-name').value !== '' && 
+					document.getElementById('enter-author').value !== '' && 
+					document.getElementById('enter-pages').value !== '');
+} 
+
+function addBookToLibrary() {
+	if (formVerify()) { // prevents form with missing fields from being submitted
+		const read = document.getElementById('read-book');
+		const book = new Book(document.getElementById('enter-name').value,
+								 document.getElementById('enter-author').value,
+								 Number(document.getElementById('enter-pages').value),
+								 Boolean(Number(read.options[read.selectedIndex].value)));
+		library.push(book); // add book to library
+		render(); // render updated local storage
+	} else {
+		alert('Please fill out all the fields!');
+	}
+	document.getElementById('new-book').reset(); // clear form
 }
 
 addBookBtn.addEventListener('click', addBookToLibrary);
 
-function formVerify() {
-    // prevents form with empty fields from being submitted
-    return (document.getElementById('enter-name').value !== '' && 
-            document.getElementById('enter-author').value !== '' && 
-            document.getElementById('enter-pages').value !== '');
-} 
-
-function findIndex(id) {
-    for (const element of library) {
-        if (id === element._id) return library.indexOf(element);
-    }
+function removeBookFromLibrary(index) {
+	library.splice(index, 1);
+	store.clear(); 
+	// if store isn't cleared before render, deleted book will be added to library again from store
+	render(); 
 }
 
-function changeReadStatus(book_id) {
-    const index = findIndex(book_id);
-    library[index].toggleReadStatus();
-    populateStorage();
-    libraryDisplay.innerHTML = '';
+function findIndex(id) {
+	for (const element of library) {
+			if (id === element._id) return library.indexOf(element);
+	}
+}
+
+function changeReadStatus(id) {
+	const index = findIndex(id);
+    library[index].read = !library[index].read;
+    store.clear();
     render(); 
 }
